2baab6ce6e319dc24740a2b8c5d8d00bee7301a4
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-07-27 17:50:32 -0300

    base user controller + testing

---
+++ backend/DTO/User.cs
@@ -0,0 +1,1 @@
+public record User(string name, string password, string? pic_url);
--- backend/api/UserController.cs
+++ backend/api/UserController.cs
@@ -4,13 +4,20 @@ using Microsoft.AspNetCore.Mvc;
 [Route("/api")]
 public class UserController : ControllerBase
 {
-    [HttpGet("test")]
-    public IResult GetUser()
+    [HttpGet("user")]
+    public IResult GetUser([FromQuery] string? name)
     {
         return Results.Ok(new
         {
-            Name = "Test"
+            Name = "Test",
+            Query = name
         });
     }
     
+    // testing query and body
+    [HttpPost("user")]
+    public IActionResult PostUser([FromBody] User user, [FromQuery] string? q)
+    {
+        return Ok(new {user, q});
+    }
 }
